d9a3470200c89d882be73028c3297036470593a7
JullMol <dimasrafiizzulhaq@gmail.com>
2024-12-03 12:46:40 +0700

    a

--- webb/__init__.py
+++ webb/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from .database import db
+import os
 
 app = Flask(__name__)
+DB_name = 'local_database.db'
 def create_app():
+    # Konfigurasi database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_name}'  # Menggunakan SQLite
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Inisialisasi SQLAlchemy
+    db.init_app(app)
+    
     from .home import home
     from .timer import timer
     from .group import group
@@ -12,4 +21,12 @@ def create_app():
     app.register_blueprint(group)
     app.register_blueprint(todo)
     app.register_blueprint(invite)
+    
+    create_base(app)
     return app
+
+def create_base(app):
+    # Membuat tabel jika belum ada
+    if not os.path.exists('webb/{DB_name}'):
+        with app.app_context():
+            db.create_all()  # Membuat semua tabel, termasuk tabel 'members'
---
+++ webb/database.py
@@ -0,0 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import time
+
+# Inisialisasi SQLAlchemy
+db = SQLAlchemy()
+
+# Model Database
+class Timer(db.Model):
+    __tablename__ = 'timers'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    task = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(50), unique=True, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    
+class ToDoListD(db.Model):
+    __tablename__ = 'todo_listsD'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    placement = db.Column(db.String(50), nullable=False)
+    activities = db.Column(db.String(50), nullable=False)
+    priority = db.Column(db.String(50), nullable=False)
+
+class ToDoListA(db.Model):
+    __tablename__ = 'todo_listsA'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    placement = db.Column(db.String(50), nullable=False)
+    activities = db.Column(db.String(50), nullable=False)
+    priority = db.Column(db.String(50), nullable=False)
+
+class ToDoListE(db.Model):
+    __tablename__ = 'todo_listsE'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    placement = db.Column(db.String(50), nullable=False)
+    activities = db.Column(db.String(50), nullable=False)
+    priority = db.Column(db.String(50), nullable=False)
+
+class Member(db.Model):
+    __tablename__ = 'members'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
