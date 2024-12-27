b8348d90b35e35de2bb4a5a84442feda7d25011c
JullMol <dimasrafiizzulhaq@gmail.com>
2024-12-27 12:32:35 +0700

    m

--- webb/database.py
+++ webb/database.py
@@ -5,22 +5,43 @@ from flask_login import UserMixin
 # Inisialisasi SQLAlchemy
 db = SQLAlchemy()
 
-# class User(db.Model, UserMixin):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(255), unique=True, nullable=False)
-#     email = db.Column(db.String(255), unique=True, nullable=False)
-#     password = db.Column(db.String(255), nullable=False)
-#     email_notif = db.Column(db.JSON, nullable=True, default={})
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email_notif = db.Column(db.JSON, nullable=True, default={})
     
-#     timers = db.relationship('Timer', backref='user', lazy=True,
-#                            foreign_keys='Timer.user_id')
+    timers = db.relationship('Timer', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='Timer.user_id')
+    goals = db.relationship('GGoals', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='GGoals.user_id')
+    project = db.relationship('pproject', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='pproject.user_id')
+    calendar = db.relationship('ccalendar', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='ccalendar.user_id')
+    notesD = db.relationship('notes_data', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='notes_data.user_id')
+    ToDoListD = db.relationship('todo_listsD', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='todo_listsD.user_id')
+    ToDoListA = db.relationship('todo_listsA', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='todo_listsA.user_id')
+    ToDoListE = db.relationship('todo_listsE', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='todo_listsE.user_id')
+    Member = db.relationship('members', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='members.user_id')
+    Schedule = db.relationship('schedule', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='schedule.user_id')
+    Record = db.relationship('record', backref='user', lazy=True, cascade= "all, delete-orphan",
+                           foreign_keys='record.user_id')
 
 # Model Database
 class Timer(db.Model):
     __tablename__ = 'timers'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_timer_user_id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
@@ -32,6 +53,7 @@ class GGoals(db.Model):
     __tablename__ = 'goals'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_goals_user_id'), nullable=False)
     your_goals = db.Column(db.String, nullable=False)
     session = db.Column(db.String, nullable=False)
     time_to_achieve_goals = db.Column(db.Date, nullable=False)
@@ -40,6 +62,7 @@ class pproject(db.Model):
     __tablename__ = 'project'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_project_user_id'), nullable=False)
     project_name = db.Column(db.String, nullable=False)
     session2 = db.Column(db.String, nullable=False)
     time_created = db.Column(db.DateTime, nullable=False)
@@ -49,6 +72,7 @@ class ccalendar(db.Model):
     __tablename__ = 'calendar'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_calendar_user_id'), nullable=False)
     name =  db.Column(db.String, nullable=False)
     colour = db.Column(db.String, nullable=False)
     date = db.Column(db.JSON)
@@ -57,6 +81,7 @@ class notes_data(db.Model):
     __tablename__ = 'notesD'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_notesD_user_id'), nullable=False)
     hari =  db.Column(db.String, nullable=False)
     content = db.Column(db.Text)
     photo = db.Column(db.JSON)
@@ -65,6 +90,7 @@ class ToDoListD(db.Model):
     __tablename__ = 'todo_listsD'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_todo_listsD_user_id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     time = db.Column(db.Time, nullable=False)
     placement = db.Column(db.String(50), nullable=False)
@@ -75,6 +101,7 @@ class ToDoListA(db.Model):
     __tablename__ = 'todo_listsA'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_todo_listsA_user_id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     time = db.Column(db.Time, nullable=False)
     subject = db.Column(db.String(50), nullable=False)
@@ -85,6 +112,7 @@ class ToDoListE(db.Model):
     __tablename__ = 'todo_listsE'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_todo_listsE_user_id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     time = db.Column(db.Time, nullable=False)
     location = db.Column(db.String(50), nullable=False)
@@ -95,6 +123,7 @@ class Member(db.Model):
     __tablename__ = 'members'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_members_user_id'), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     phone = db.Column(db.String(20), nullable=False)
@@ -105,6 +134,7 @@ class Schedule(db.Model):
     __tablename__ = 'schedule'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_schedule_user_id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     subject = db.Column(db.String(50), nullable=False)
     link = db.Column(db.String, unique=True, nullable=False)
@@ -112,6 +142,7 @@ class Schedule(db.Model):
 class Record(db.Model):
     __tablename__ = 'record'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_record_user_id'), nullable=False)
     channel_type = db.Column(db.String(50), nullable=False)
     channel_name = db.Column(db.String(100), nullable=False)
     link = db.Column(db.String(255), nullable=False)
